66bb17af01b531fbfed93c9083e7347bde81cd62
OmarAstro <145227504+OmarAstro@users.noreply.github.com>
2024-07-17 23:23:31 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = ({setShowLogin}) => {
       <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className="navbar-menu">
         <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
-        <Link to='/food/#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</Link>
-        <Link to='/food/#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</Link>
-        <Link to='/food/#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</Link>
+        <a href='/food/#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+        <a href='/food/#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
